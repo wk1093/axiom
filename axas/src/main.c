1e6a15a5b388a2a11d7a1b3426e7a765ac75fb42
Wyatt Kloos <Wyatt.Kloos@mines.sdsmt.edu>
2026-03-31 16:02:52 -0600

    assember can take multiple files now

--- axas/src/main.c
+++ axas/src/main.c
@@ -11,45 +11,29 @@
 #include "axas_parser.h"
 #include "axas_assembler.h"
 
-// New assembler entry point with command line
-// Options:
-// axas <input_file> [-o <output_file>] [--lex] [--parse]
-// If --lex is provided, print tokens and exit
-// If --parse is provided, print parsed units and exit
 int main(int argc, char** argv) {
     if (argc == 1) {
-        printf("Usage: %s <input_file> [-o <output_file>] [--lex] [--parse]\n", argv[0]);
+        printf("Usage: %s <input_file>... [-o <output_file>]\n", argv[0]);
         printf("Options:\n");
-        printf("  --lex       Print tokens and exit\n");
-        printf("  --parse     Print parsed units and exit\n");
-        printf("  -o <file>   Specify output file (default is input file with .o extension)\n");
+        printf("  -o <file>   Specify output file (default: first input with .o extension)\n");
         return 1;
     }
 
-    const char* filename = NULL;
-    bool do_lex = false;
-    bool do_parse = false;
+    const char** input_files = malloc(argc * sizeof(char*));
+    int input_count = 0;
     char* output_filename = NULL;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--lex") == 0) {
-            do_lex = true;
-        } else if (strcmp(argv[i], "--parse") == 0) {
-            do_parse = true;
-        } else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) {
+        if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) {
             output_filename = argv[++i];
         } else {
-            filename = argv[i];
+            input_files[input_count++] = argv[i];
         }
     }
 
-    if (!filename) {
-        printf("Error: No input file specified.\n");
-        return 1;
-    }
-
-    if (do_lex && do_parse) {
-        printf("Error: Cannot specify both --lex and --parse.\n");
+    if (input_count == 0) {
+        printf("Error: No input files specified.\n");
+        free(input_files);
         return 1;
     }
 
@@ -57,70 +41,52 @@ int main(int argc, char** argv) {
 
     if (!output_filename) {
         output_alloc = true;
-        // Default output filename is input filename with .o extension
-        size_t len = strlen(filename);
-        output_filename = malloc(len + 3); // for .o and null terminator
-        strncpy(output_filename, filename, len+1);
+        // Default: first input filename with .o extension
+        size_t len = strlen(input_files[0]);
+        output_filename = malloc(len + 3);
+        strncpy(output_filename, input_files[0], len + 1);
         char* dot = strrchr(output_filename, '.');
         if (dot) {
-            *dot = '\0'; // remove existing extension
+            *dot = '\0';
         }
         strcat(output_filename, ".o");
-    }
-    
-    FILE* f = fopen(filename, "r");
-    if (!f) {
-        perror("Failed to open input file");
-        return 1;
-    }
-
-    fseek(f, 0, SEEK_END);
-    size_t filesize = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    char* contents = malloc(filesize + 1);
-    fread(contents, 1, filesize, f);
-    contents[filesize] = '\0';
-    fclose(f);
-
-    AxLexer *lexer = ax_lexerNew(contents);
-    if (do_lex) {
-        AxToken token;
-        do {
-            token = ax_lexerNextToken(lexer);
-            ax_printToken(token);
-            if (token.type != TOK_NEWLINE) {
-                printf(" ");
-            } else {
-                printf("\n");
-            }
-        } while (token.type != TOK_EOF && token.type != TOK_ERROR);
-        printf("\n");
-        ax_lexerFree(lexer);
-        free(contents);
-        return 0;
-    }
-
-    if (do_parse) {
-        AxParsedUnit unit;
-        do {
-            unit = ax_parseUnit(lexer);
-            ax_debugUnit(unit);
-        } while (lexer->pos < strlen(contents) && unit.type != UNIT_EOF);
-        ax_lexerFree(lexer);
-        free(contents);
-        return 0;
     }
 
     AxObject obj;
     ax_objectInit(&obj);
-    ax_assemble(&obj, lexer);
+
+    for (int i = 0; i < input_count; i++) {
+        FILE* f = fopen(input_files[i], "r");
+        if (!f) {
+            fprintf(stderr, "Error: Failed to open '%s': ", input_files[i]);
+            perror(NULL);
+            ax_objectFree(&obj);
+            free(input_files);
+            if (output_alloc) free(output_filename);
+            return 1;
+        }
+
+        fseek(f, 0, SEEK_END);
+        size_t filesize = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        char* contents = malloc(filesize + 1);
+        fread(contents, 1, filesize, f);
+        contents[filesize] = '\0';
+        fclose(f);
+
+        AxLexer* lexer = ax_lexerNew(contents);
+        ax_assemble(&obj, lexer);
+        ax_lexerFree(lexer);
+        free(contents);
+    }
+
     ax_objectWrite(&obj, output_filename);
-    printf("Axiom: Generated %s with %zu instructions.\n", output_filename, ax_vecSize(obj.text));
+    printf("Axiom: Generated %s with %zu instructions from %d file(s).\n",
+           output_filename, ax_vecSize(obj.text), input_count);
     ax_objectFree(&obj);
-    ax_lexerFree(lexer);
-    free(contents);
+    free(input_files);
     if (output_alloc) {
-        free((void*)output_filename);
+        free(output_filename);
     }
 
     return 0;
